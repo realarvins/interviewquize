4ea19f408671a1fe7bccc1cea077e72354953f3e
Arvind Chauhan <aktech9990@gmail.com>
2024-03-30 15:01:50 +0530

    Interview Quiz Project Completion

---
+++ assets/js/50-react-mcq.js
@@ -0,0 +1,27 @@
+
+const correctAns = ["A","B","D","B","B","A","B","D","A","A","C","C","B","C","A","B","A","C","C","A","D","B","A","C","B","A","C","B","A","B","B","C","C","C","A","A","A","A","B","D","D","D","A","A","B","C","A","D","C","C"];
+const form = document.querySelector(".quiz-form");
+const result = document.querySelector(".result");
+const question = document.querySelectorAll(".question");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let score = 0;
+    const  userAns = [form.q1.value,form.q2.value,form.q3.value,form.q4.value,form.q5.value,form.q6.value,form.q7.value,form.q8.value,form.q9.value,form.q10.value,form.q11.value,form.q12.value,form.q13.value,form.q14.value,form.q15.value,form.q16.value,form.q17.value,form.q18.value,form.q19.value,form.q20.value,form.q21.value,form.q22.value,form.q23.value,form.q24.value,form.q25.value,form.q26.value,form.q27.value,form.q28.value,form.q29.value,form.q30.value,form.q31.value,form.q32.value,form.q33.value,form.q34.value,form.q35.value,form.q36.value,form.q37.value,form.q38.value,form.q39.value,form.q40.value,form.q41.value,form.q42.value,form.q43.value,form.q44.value,form.q45.value,form.q46.value,form.q47.value,form.q48.value,form.q49.value,form.q50.value];
+    userAns.forEach((ans , index) => {
+        if(correctAns[index] === ans){
+            score += 1;
+
+            question[index].classList.add("correct");
+        }    else{
+            question[index].classList.add("incorrect");
+        }
+    });
+    console.log(score);
+
+    scrollTo(0,0); //scroll to top  of the page after submitting the quiz
+
+    result.classList.remove("hide");
+    result.querySelector("p").textContent = `Your Scored ${score}/50`;
+});
